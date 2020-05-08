9a0119dcc758bd050e09ed2f1e88b22a2d22b3e6
K.M.H.Mubin <52270073+kmhmubin@users.noreply.github.com>
2020-05-08 21:00:09 +0600

    Basic Template

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Logo />
+      <ImageLinkForm />
+      <FaceRecognition/>
+    </div>
+  );
+}
+
+export default App;
