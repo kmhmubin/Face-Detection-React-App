57d1008665cf8234baadeba79771b86ec33ce1ae
K.M.H.Mubin <52270073+kmhmubin@users.noreply.github.com>
2020-05-08 22:54:14 +0600

    LOGO,IMAGEFROMLINK,NAVIGATION,RANK ADDED

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React from "react";
+import Particles from "react-particles-js";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import "./App.css";
 
-import './App.css';
+const ParticlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+  },
+};
 
 function App() {
   return (
     <div className="App">
+      <Particles className="particles" params={ParticlesOptions} />
+
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
-      <FaceRecognition/>
+      {/*  <FaceRecognition/> */}
     </div>
   );
 }
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Rank = () => {
+  return (
+    <div>
+      <div className="white f3">{"Jone, Your Current Rank is .. "}</div>
+      <div className="white f1">{"#5"}</div>
+    </div>
+  );
+};
+
+export default Rank;
